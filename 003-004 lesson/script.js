ea4f56a4b495b5dee6e1a74b8ac24ae04daad41b
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-02-27 23:56:24 +0300

    JS official error

--- 003-004 lesson/script.js
+++ 003-004 lesson/script.js
@@ -11,30 +11,108 @@
 
 // 004
 
-var number = 5;
-var string = "Hello!";
-var sym = Symbol();
-var boolean =true;
-null;
-undefined;
-var obj = {};
+// var number = 5;
+// var string = "Hello!";
+// var sym = Symbol();
+// var boolean =true;
+// null;
+// undefined;
+// var obj = {};
 
-console.log(4/0);
-console.log('string'*9);
+// console.log(4/0);
+// console.log('string'*9);
 
-let something;
-console.log(something);
+// let something;
+// console.log(something);
 
-let persone = {
-    name: "John",
-    age: 25,
-    isMarried: false
-};
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
 
-console.log(persone['name']);
+// console.log(persone['name']);
 
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
 
-console.log(arr[0]);
+// console.log(arr[0]);
 
 // / 004
+
+// 004 2
+
+// null;       
+// // // - манго, которого нет на столе 
+
+// // undefined; 
+// // // - существует, но не имеет никакого значения
+
+// // let a;
+// // console.log(a);
+
+// const obj = {
+//     name: 'Alex'
+// }
+// // - ключ, значение
+
+// const arr = [1, 2, 3, 4]
+            
+// //             0   1  2  3
+// // - массив спец. объект ключ 0 значение 1, ключ 3 значение 4
+
+// // 1)
+// console.log(typeof(String(null)));
+// // 2)
+// console.log(typeof("" + false));
+// console.log('http://vk.com/catalog' + 5);
+
+// // - to Number
+
+// // 1)
+// console.log(typeof(Number('5')));
+// // 2)
+// console.log(typeof(5 + +'5'));
+// // 3)
+// console.log(typeof(parseInt('15px', 10)));
+
+// // let ans = +prompt("Hello?", '');
+
+// // 0, '', null, undefined, NaN - это все всегда будет false, остальное true
+
+// let switcher = null;
+
+// if (switcher) {
+//     console.log("Working...")
+// }
+
+// switcher = 1;
+
+// if (switcher) {
+//     console.log("Working...")
+// }
+
+// // 2)
+// console.log(typeof(Boolean('5')));
+// // 3)
+// console.log(typeof(!!'5'));
+
+// / 004 2
+
+// 005
+
+// alert("Hello World!");
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+
+// let answer = prompt("Есть ли Вам 18?", "Да");
+
+// console.log(typeof(arr));
+// console.log(typeof(null)); 
+// - Object это официальная ошибка JS, так как null - это отдельный тип данных
+
+
+// / 005
