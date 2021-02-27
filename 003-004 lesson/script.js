88b2ea37edcf88a4c132b25551ec4f6ace00e55c
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-02-27 22:53:46 +0300

    start

--- 003-004 lesson/script.js
+++ 003-004 lesson/script.js
@@ -1,10 +1,40 @@
 'use strict';
 
-
+// 003
 // console.log(leftBorderWidth);
 
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
 
+// / 003
 
+// 004
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean =true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone['name']);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+// / 004
