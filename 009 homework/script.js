f0cbb97d4f4ad9a2ca8ed21d6e9a98ea156451a2
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-03-01 00:15:34 +0300

    homework 009

--- 009 homework/script.js
+++ 009 homework/script.js
@@ -1,1 +1,59 @@
 'use strict';
+
+// Quations:
+
+// 1) 5
+
+// let x = 5;
+
+// alert(x++);
+
+// 2) NaN
+
+// [] + false - null + true
+
+// 3) 2
+
+// let y = 1;
+// let x = y = 2;
+
+// console.log(x);
+
+// 4) "—" 12 
+
+// [] + 1 + 2;
+
+// 5) 1
+
+// console.log("1"[0]);
+
+// 6) null
+
+// console.log(2 && 1 && null && 0 && undefined)
+
+// 7) Yes
+
+// !!(a && b) и (a && b)
+
+// 8) "-" 3
+
+// console.log(null || 2 && 3 || 4);
+
+// 9) "—" No
+
+// let a = [1, 2, 3];
+// let b = [1, 2, 3];
+
+// (a == b) ???
+
+// 10) Infinity
+
+// console.log(+"Infinity");
+
+// 11) true
+
+// console.log(Boolean("ёжик" > "яблоко"));
+
+// 12) "—" 2
+
+// console.log( 0 || "" || 2 || undefined || true || falsе)
