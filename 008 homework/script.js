acf11036e0008dc63b608f03ba4df071d82266d9
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-02-28 22:02:03 +0300

    008 homework

---
+++ 008 homework/script.js
@@ -0,0 +1,15 @@
+'use strict';
+// Homework
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+        budget: money,
+        timeData: time,
+        expenses: ,
+        optionalExpenses: ,
+        saving: false
+    };
+
+
+// / Homework
