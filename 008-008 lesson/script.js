548438ab566a1cf7b9acd42454c736e1079e01ad
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-02-28 21:50:18 +0300

    homework less 008]

--- 008-008 lesson/script.js
+++ 008-008 lesson/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+// if(2*4 === 8) {
+//     console.log("Верно")
+// }
+
+// let num = 50;
+
+// if (num < 49) {
+//     console.log('Неверно')
+// } else if (num > 100) {
+//     console.log("Много!")
+// } else {
+//     console.log("Верно!")
+// };
+
+// (num == 50) ? console.log('Верно!') :console.log('Неверно!')
+
+// switch (num) {
+//     case num < 49:
+//         console.log("Неверно");
+//         break;
+//     case num > 100:
+//         console.log("Много!");
+//         break;
+//     case num > 80:
+//         console.log("Все еще много!");
+//         break;
+//     case 50:
+//         console.log("Верно!");
+//         break;
+//     default:
+//         console.log("Что-то пошло не так!");
+//         break;
+// }
