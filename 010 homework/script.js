031ebf5c0fc205bedfa14d398e17062338b4094e
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-03-27 19:18:59 +0300

    homework

---
+++ 010 homework/script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?");
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?");
+    }
+}
+start();
+
+
+
+
+let appData = {
+        budget: money,
+        timeData: time,
+        expenses: {},
+        optionalExpenses: {},
+        income: [],
+        savings: true
+    };
+
+function chooseExpenses() {
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = prompt('Во сколько обойдется?', '')
+
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+        && a != '' && b != '' && a.length < 50) {
+        appData.expenses[a] = b;
+    } else {
+
+    }
+}
+}
+chooseExpenses();
+
+
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
+
+function checkSavings() {
+    if(appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            persent = +prompt("Под какой процент?");
+
+        appData.monthInCome = save/100/12*persent;
+        alert("Доход в месяц с вашего депозита: " + appData.monthInCome);
+    }
+}
+checkSavings();
+
+function dete
+appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
--- 010 lesson/script.js
+++ 010 lesson/script.js
@@ -1,10 +1,42 @@
 'use strict';
-let num = 20;
 
-function showFirstMessage(text) {
-    alert(text);
-    console.log(num);
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     alert(text);
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello World!");
+// console.log(num);
+
+
+// let calc = function calc(a, b) {
+//     return (a + b);
+// }
+
+let calc = (a,b) => a+b
+
+console.log(calc(3, 4));
+
+console.log(calc(8, 4));
+
+function retVar() {
+    let num = 50;
+    return num
 }
 
-showFirstMessage("Hello World!");
-console.log(num);
+let anotgerNum = retVar();
+console.log(anotgerNum);
+
+let str = "test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.2";
+console.log(Math.round(twelve));
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
