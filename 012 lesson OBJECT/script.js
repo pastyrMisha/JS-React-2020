ed06c45e0f90921955ea6320f9d88395ddfb5af4
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-04-09 16:53:40 +0300

    done

--- 012 lesson OBJECT/script.js
+++ 012 lesson OBJECT/script.js
@@ -3,3 +3,19 @@ let options = {
     height: 1024,
     name: "test"
 };
+console.log(options.name);
+options.bool = false;
+options.colors = {
+    border: "black",
+    bg: "red"
+};
+
+delete options.bool;
+
+
+console.log(options);
+
+for (let key in options) {
+    console.log('Свойство ' + key + ' имеет значение ' + options[key])
+}
+console.log(Object.keys(options).length);
