edae98450c10d3c3917d9e15af9fb521385212dc
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-04-09 15:05:02 +0300

    ref_done

--- 010 homework/script.js
+++ 010 homework/script.js
@@ -4,8 +4,8 @@
 let money, time;
 
 function start() {
-    money = +prompt("Ваш бюджет на месяц?");
-    time = prompt("Введите дату в формате YYYY-MM-DD");
+    money = +prompt("Ваш бюджет на месяц?", '');
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
     while(isNaN(money) || money == "" || money == null) {
         money = +prompt("Ваш бюджет на месяц?");
