1ec4cba3ec8e73fb4dc32ce2c121fb971f4d3e0a
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-01 20:01:40 +0300

    ref

--- 022 lesson TIMER/js/script.js
+++ 022 lesson TIMER/js/script.js
@@ -38,6 +38,42 @@ window.addEventListener('DOMContentLoaded', function () {
 
 // Timer
 
-let deadline = '';
-   
+let deadline = '2021-10-21';
+    
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+            // hours = Math.floor((t/1000*60/60) % 24),
+            // days = Math.floor((t/(1000*60*60*2 4)));
+
+            return {
+              'total' : t,
+              'hours' : hours,
+              'minutes' : minutes,
+              'seconds' : seconds  
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if(t.total < 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    };
+
+    setClock('timer', deadline);
 });
