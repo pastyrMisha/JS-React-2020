9581719cc7e4c744006494dd9f08bee424ff3873
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-11 22:38:26 +0300

    welldone

--- 023 homework MODAL WINDOW/js/myscript.js
+++ 023 homework MODAL WINDOW/js/myscript.js
@@ -1,40 +1,24 @@
 let age = document.getElementById('age');
 
-function User(surname, name) {
-    this.name = name;
-    this.surname = surname;
-    this.hello = function showUser() {
-        alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
-    };
-}
-User.prototype.age = age.value;
+function showUser(surname, name) {
+    alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+  };
 
+ let double = showUser.bind(age);
 
-let Mike = new User('Михаил', 'Мишустин');
-
-console.log(Mike);
-Mike.hello();
+double('Михаил', 'Мишустин');
 
 
 
-// function User(name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-//     this.hello = function() {
-//         console.log('Hello! ' + this.name);
-//     }
+
+// // Вариант из интернета
+
+// let age = document.getElementById('age');
+ 
+// function showUser(surname, name) {
+//          alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 // }
-// User.prototype.exit = function(name) {
-//     console.log('Пользователь ' + this.name + ' ушел');
-// }
+ 
+// showUser.apply(age, ["Горький","Максим"]);
 
-// let ivan = new User('Ivan', 25),
-//     alex = new User('Alex', 25);
-
-// console.log(ivan);
-// console.log(alex);
-
-// alex.hello();
-// ivan.exit();
 
