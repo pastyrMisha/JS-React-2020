5062360c6d882550f1b8fc3ad612e144252b95b3
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-11 22:41:12 +0300

    comment

--- 023 homework MODAL WINDOW/js/myscript.js
+++ 023 homework MODAL WINDOW/js/myscript.js
@@ -4,9 +4,9 @@ function showUser(surname, name) {
     alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
   };
 
- let double = showUser.bind(age);
+ let double = showUser.bind(age); // К кому привязать и (что)
 
-double('Михаил', 'Мишустин');
+double('Михаил', 'Мишустин'); // теперь дабл — это ф-ия и тут (указываем остальные параметры функции, к которой age привязали) 
 
 
 
