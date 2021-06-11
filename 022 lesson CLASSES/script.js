881717369834bc4f67e5d963a297c76e20edf7dd
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-11 22:04:09 +0300

    ref

--- 022 lesson CLASSES/script.js
+++ 022 lesson CLASSES/script.js
@@ -22,3 +22,22 @@
 
 
 // ES6
+class User {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;
+    }
+    hello() {
+        console.log(`Hello! ${this.name}`)
+    }
+    exit() {
+        console.log(`Пользователь ${this.name} ушел`)
+    }
+}
+let ivan = new User('Ivan', 25);
+alex = new User('Alex', 25);
+console.log(ivan);
+console.log(alex);
+ivan.hello();
+alex.hello();
--- 023 homework MODAL WINDOW/js/myscript.js
+++ 023 homework MODAL WINDOW/js/myscript.js
@@ -1,39 +1,40 @@
 let age = document.getElementById('age');
 
-// function User(surname, name) {
+function User(surname, name) {
+    this.name = name;
+    this.surname = surname;
+    this.hello = function showUser() {
+        alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+    };
+}
+User.prototype.age = age.value;
+
+
+let Mike = new User('Михаил', 'Мишустин');
+
+console.log(Mike);
+Mike.hello();
+
+
+
+// function User(name, id) {
 //     this.name = name;
-//     this.surname = surname;
-//     this.hello = function showUser() {
-//         alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
-//     };
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log('Hello! ' + this.name);
+//     }
+// }
+// User.prototype.exit = function(name) {
+//     console.log('Пользователь ' + this.name + ' ушел');
 // }
 
+// let ivan = new User('Ivan', 25),
+//     alex = new User('Alex', 25);
 
-// let Mike = new User('Михаил', 'Мишустин');
+// console.log(ivan);
+// console.log(alex);
 
-// console.log(Mike);
-// Mike.hello();
-
-
-
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log('Hello! ' + this.name);
-    }
-}
-User.prototype.exit = function(name) {
-    console.log('Пользователь ' + this.name + ' ушел');
-}
-
-let ivan = new User('Ivan', 25),
-    alex = new User('Alex', 25);
-
-console.log(ivan);
-console.log(alex);
-
-alex.hello();
-ivan.exit();
+// alex.hello();
+// ivan.exit();
 
