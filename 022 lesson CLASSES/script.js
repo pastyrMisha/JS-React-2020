d5123698133fbaaa2daaef43e43793aa59df0451
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-11 20:49:10 +0300

    ref

--- 022 lesson CLASSES/script.js
+++ 022 lesson CLASSES/script.js
@@ -12,7 +12,7 @@
 //     console.log('Пользователь ' + this.name + ' ушел');
 // }
 
-// let ivan = new User('Ivan', 25);
+// let ivan = new User('Ivan', 25),
 //     alex = new User('Alex', 25);
 
 // console.log(ivan);
@@ -22,22 +22,3 @@
 
 
 // ES6
-class User {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
-        this.human = true;
-    }
-    hello() {
-        console.log(`Hello! ${this.name}`)
-    }
-    exit() {
-        console.log(`Пользователь ${this.name} ушел`)
-    }
-}
-let ivan = new User('Ivan', 25);
-alex = new User('Alex', 25);
-console.log(ivan);
-console.log(alex);
-ivan.hello();
-alex.hello();
---
+++ 023 homework MODAL WINDOW/js/myscript.js
@@ -0,0 +1,39 @@
+let age = document.getElementById('age');
+
+// function User(surname, name) {
+//     this.name = name;
+//     this.surname = surname;
+//     this.hello = function showUser() {
+//         alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+//     };
+// }
+
+
+// let Mike = new User('Михаил', 'Мишустин');
+
+// console.log(Mike);
+// Mike.hello();
+
+
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log('Hello! ' + this.name);
+    }
+}
+User.prototype.exit = function(name) {
+    console.log('Пользователь ' + this.name + ' ушел');
+}
+
+let ivan = new User('Ivan', 25),
+    alex = new User('Alex', 25);
+
+console.log(ivan);
+console.log(alex);
+
+alex.hello();
+ivan.exit();
+
