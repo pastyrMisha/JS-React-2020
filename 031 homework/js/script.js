02e47e6e3d7c2a354c41832af9a6709f031c26ef
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-14 00:18:42 +0300

    куа

--- 031 homework/js/script.js
+++ 031 homework/js/script.js
@@ -133,7 +133,7 @@ form.addEventListener('submit', function(event) {
 
     let request = new XMLHttpRequest();
     request.open('POST', 'server.php');
-    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
     let formData = new FormData(form);
     request.send(formData);
