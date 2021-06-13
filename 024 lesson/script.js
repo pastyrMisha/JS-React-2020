b86c89ed5f65d904fcc85c6062149618267ce8e0
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-13 09:35:05 +0300

    done

--- 024 lesson/script.js
+++ 024 lesson/script.js
@@ -2,5 +2,5 @@ let name = "Ivan",
     age = 30,
     mail = 'ex@mail.ru';
 
-// document.write('Пользователю ' + name + ' ' + age + ' лет. Его почтовый адрес ' + mail);
-document.write(`Пользователю ${name} ${age} лет. Его почтовый адрес ${mail}`);
+// document.write('Пользователю ' + name + ' ' + age + ' лет. Его почтовый адрес: ' + mail);
+document.write(`Пользователю ${name} ${age} лет. Его почтовый адрес: ${mail}`);
