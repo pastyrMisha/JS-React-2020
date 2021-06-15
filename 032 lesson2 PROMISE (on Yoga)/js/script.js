5af81158e24753d95872e6df4f35b6f213575290
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-15 22:06:41 +0300

    welldone

--- 032 lesson2 PROMISE (on Yoga)/js/script.js
+++ 032 lesson2 PROMISE (on Yoga)/js/script.js
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
         descBtn = document.querySelectorAll('.description-btn'),
         deadline = '2021-10-21';
 
-// Tabs
+    // Tabs
     const hideTabContent = (a) => {
             for (let i = a; i < tabContent.length; i++) {
                 tabContent[i].classList.remove('show');
@@ -43,7 +43,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-// Timer
+    // Timer
     const getTimeRemaining = (endtime) => {
             let t = Date.parse(endtime) - Date.parse(new Date()),
                 seconds = Math.floor((t / 1000) % 60),
@@ -89,7 +89,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('timer', deadline);
 
-// Modal 
+    // Modal 
     more.addEventListener('click', () => {
         overlay.style.display = 'block';
         more.classList.add('more-splash');
@@ -114,71 +114,82 @@ window.addEventListener('DOMContentLoaded', () => {
             descBtn.classList.remove('more-splash');
         })
     });
-// Form
+    // Form
     let message = {
         loading: 'Загрузка',
         success: 'Спасибо! Скоро мы с вами свяжемся!',
         failure: 'Что-то пошло не так!'
     };
 
-    let form = document.getElementsByClassName('main-form')[0],
-    contactForm = document.getElementById('form'),
+    let form = document.querySelector('.main-form'),
+        contactForm = document.getElementById('form'),
         input = document.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
-        
-        statusMessage.classList.add('status');
 
-function sendForm(elem) {
+    statusMessage.classList.add('status');
+
+    function sendForm(elem) {
 
 
-    elem.addEventListener('submit', function(e) {
-        e.preventDefault();
-        elem.appendChild(statusMessage);
-        // Сначала, при помощи объекта FormData получаем все, что ответил наш пользователь в форме 
-        let formData = new FormData(elem);
+        elem.addEventListener('submit', function (e) {
+            e.preventDefault();
+            elem.appendChild(statusMessage);
+            // Сначала, при помощи объекта FormData получаем все, что ответил наш пользователь в форме 
+            let formData = new FormData(elem);
 
-        function postData(data) {
-            return new Promise(function(resolve, reject) {
-                let request = new XMLHttpRequest();
-                request.open('POST', 'server.php');
-                request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            function postData() {
+                return new Promise(function (resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
-                request.onreadystatechange = function() {
-                    if(request.readyState < 4) {
-                        resolve()
-                    } else if (request.readyState === 4) {
-                        if (request.status == 200 && request.status < 3) 
+                    request.onreadystatechange = function () {
+                        if (request.readyState < 4) {
                             resolve()
+                        } else if (request.readyState === 4) {
+                            if (request.status == 200 && request.status < 3)
+                                resolve()
+                        } else {
+                            reject()
+                        }
                     }
-                    else {
-                        reject()
-                    }
+
+                    let formData = new FormData(elem);
+
+                    let obj = {};
+
+                    formData.forEach(function (value, key) {
+                        obj[key] = value;
+                    });
+
+                    let json = JSON.stringify(obj);
+
+                    request.send(json);
+
+                }) // End Promise
+            } // End postData
+
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
                 }
-
-                request.send(data);
-            }) // End Promise
-        } // End postData
-
-        function clearInput() {
-            for (let i = 0; i < input.length; i++) {
-                input[i].value = '';
             }
-        }
 
-        postData(formData)
-        .then(() => statusMessage.innerHTML = message.loading)
-        .then(() => {
-            thanksModal.style.display = 'block';
-            mainModal.style.display = 'none';
-            statusMessage.innerHTML = '';
-        })
-        .catch(() => statusMessage.innerHTML = message.failure)
-        .then(clearInput)
+            postData(formData)
+                .then(() => statusMessage.innerHTML = message.loading)
+                .then(() => {
+                    // thanksModal.style.display = 'block';
+                    // mainModal.style.display = 'none';
+                    // statusMessage.innerHTML = '';
+                    statusMessage.innerHTML = message.success;
+                })
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(clearInput)
 
-    }) //End EventListener on submit
-} // End sendForm
-sendForm(form);
-sendForm(contactForm);
+        }) //End EventListener on submit
+    } // End sendForm
+    sendForm(form);
+    sendForm(contactForm);
 
 
 
