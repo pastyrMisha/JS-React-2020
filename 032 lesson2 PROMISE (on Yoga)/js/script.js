2a7f116de0aa97c2efcc4eb87ad612b8a019e8c2
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-15 16:10:59 +0300

    start

--- 032 lesson2 PROMISE (on Yoga)/js/script.js
+++ 032 lesson2 PROMISE (on Yoga)/js/script.js
@@ -13,7 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
         descBtn = document.querySelectorAll('.description-btn'),
         deadline = '2021-10-21';
 
-    // Tabs
+// Tabs
     const hideTabContent = (a) => {
             for (let i = a; i < tabContent.length; i++) {
                 tabContent[i].classList.remove('show');
@@ -43,7 +43,7 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Timer
+// Timer
     const getTimeRemaining = (endtime) => {
             let t = Date.parse(endtime) - Date.parse(new Date()),
                 seconds = Math.floor((t / 1000) % 60),
@@ -89,7 +89,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('timer', deadline);
 
-    // Modal 
+// Modal 
     more.addEventListener('click', () => {
         overlay.style.display = 'block';
         more.classList.add('more-splash');
@@ -114,67 +114,69 @@ window.addEventListener('DOMContentLoaded', () => {
             descBtn.classList.remove('more-splash');
         })
     });
-    // Form
+// Form
     let message = {
         loading: 'Загрузка',
         success: 'Спасибо! Скоро мы с вами свяжемся!',
         failure: 'Что-то пошло не так!'
     };
 
-    let form = document.querySelectorAll('.main-form'),
+    let form = document.querySelector('.main-form'),
         input = document.getElementsByTagName('input'),
-        statusMessage = document.createElement('div');
+        statusMessage = document.createElement('div'),
+        contactForm = document.getElementById('form');
 
-    statusMessage.classList.add('status');
+        statusMessage.classList.add('status');
 
-    form.forEach((form) => {
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        form.appendChild(statusMessage);
+let sendForm = (elem) => {
 
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    elem.addEventListener('submit', function(event) {
+    event.preventDefault();
+    elem.appendChild(statusMessage);
 
-
-        // ====
-        // Сначала, при помощи объекта FormData получаем все, что ответил наш пользователь в форме 
-        let formData = new FormData(form);
-
-        // Создаем новый объект, в который мы поместим все эти данные
-        let obj = {};
-
-        // С помощью метода forEach мы берем наш объект FormData и все данные, которые есть в нем помещаем в этот объект obj
-        formData.forEach(function (value, key) {
-            obj[key] = value;
-        });
-
-        // Осталось только превратить объект obj в json формат, при помощи одного из двух методов
-        let json = JSON.stringify(obj);
-
-        // Отправляем json на сервер
-        request.send(json);
-        // ====
+    let request = new XMLHttpRequest();
+    request.open('POST', 'server.php');
+    request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
 
-        request.addEventListener('readystatechange', function () {
-            if (request.readyState < 4) {
-                statusMessage.innerHTML = message.loading;
-            } else if (request.readyState === 4 && request.status == 200) {
-                statusMessage.innerHTML = message.success;
-            } else {
-                statusMessage.innerHTML = message.failure;
-            }
-        });
+// ====
+    // Сначала, при помощи объекта FormData получаем все, что ответил наш пользователь в форме 
+    let formData = new FormData(elem);
+    
+    // Создаем новый объект, в который мы поместим все эти данные
+    let obj = {}; 
 
-        for (let i = 0; i < input.length; i++) {
-            input[i].value = '';
+    // С помощью метода forEach мы берем наш объект FormData и все данные, которые есть в нем помещаем в этот объект obj
+    formData.forEach(function(value, key) { 
+        obj[key] =  value;
+    });
+
+    // Осталось только превратить объект obj в json формат, при помощи одного из двух методов
+    let json = JSON.stringify(obj); 
+    
+    // Отправляем json на сервер
+    request.send(json); 
+// ====
+
+
+    request.addEventListener('readystatechange', function() {
+        if(request.readyState < 4) {
+            statusMessage.innerHTML = message.loading;
+        } else if(request.readyState === 4 && request.status == 200) {
+            statusMessage.innerHTML = message.success;
+        } else {
+            statusMessage.innerHTML = message.failure;
         }
     });
+
+    for (let i = 0; i < input.length; i++) {
+        input[i].value = '';
+    }
 });
-    // ------------- Promise
 
-
-
+};
+sendForm(form);
+sendForm(contactForm);
+// ------------- HomeWork
 
 });
