ffcbc821e9bb6e326750381dcd891e1a8720816e
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-15 13:14:41 +0300

    start promise

--- 032 PROMISE/promise.js
+++ 032 PROMISE/promise.js
@@ -1,20 +1,3 @@
-// Polyfill
-// https://babeljs.io/docs/en/babel-polyfill/
-
-// btn.addEventListener('click', function() {
-//     alert("Что-то произошло");
-// });
-
-// let func1 = function(param, func2) {
-//     func2(function(param, func3) {
-//         func3(function(param, func4) {
-//             func4(function(param, func5) {
-
-//             });
-//         });
-//     });
-// };
-
 let drink = 0;
 
 function shoot(arrow, headshot, fail) {
