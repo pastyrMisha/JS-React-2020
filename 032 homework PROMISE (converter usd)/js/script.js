7c0d0d65de51328e85dc7513593374a21385e8cf
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-17 23:38:46 +0300

    process

--- 032 homework PROMISE (converter usd)/js/script.js
+++ 032 homework PROMISE (converter usd)/js/script.js
@@ -1,35 +1,45 @@
 let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
-
-
 inputRub.addEventListener('input', () => {
+
+
+    let request = new XMLHttpRequest();
+    
+
+
     function getData() {
         return new Promise(function (resolve, reject) {
-            let request = new XMLHttpRequest();
+
             request.open('GET', 'js/current.json');
             request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-           
+            
+
 
             request.onreadystatechange = function () {
-                if (request.readyState <= 4) {
+                if (request.readyState < 4) {
+                    resolve()
+                } else if (request.readyState === 4) {
                     if (request.status == 200 && request.status < 3)
                         resolve()
                 } else {
                     reject()
                 }
-            }
+            };
+
+            
+        
+
             request.send();
+            
         });
+
     };
+
     getData()
+    
         .then(() => {
-            let data = JSON.parse(request.response);
-            inputUsd.value = inputRub.value / data.usd;
-            console.log('nzr');
+            inputUsd.value = "Что-то пошло  так!"
         })
-        .catch(() => inputUsd.value = "Что-то пошло не так!")
+        .catch(() =>  inputUsd.value = "Что-то пошло не так!")
 });
-
-
-// 
