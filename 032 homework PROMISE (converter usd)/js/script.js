32d937fcbe3c760cec1e770cdc40b5da085066bb
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-18 01:30:19 +0300

    rf

--- 032 homework PROMISE (converter usd)/js/script.js
+++ 032 homework PROMISE (converter usd)/js/script.js
@@ -1,45 +1,64 @@
-let inputRub = document.getElementById('rub'),
-    inputUsd = document.getElementById('usd');
 
-inputRub.addEventListener('input', () => {
+// let inputRub = document.getElementById('rub'),
+// inputUsd = document.getElementById('usd');
 
 
-    let request = new XMLHttpRequest();
+
+
+
+// inputRub.addEventListener('input', () => {
+// let request = new XMLHttpRequest();
+
+
+// function getData() {
+
+//     return new Promise(function (resolve, reject) {
+
+
+// request.open('GET', 'js/current.json');
+// request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+// request.send();
+
+
+
+// request.addEventListener('readystatechange', function() {
+//     if (request.readyState === 4 && request.status == 200) {
+//         // let data = JSON.parse(request.response);
+//         // inputUsd.value = inputRub.value / data.usd;
+//         resolve()
+//     } else {
+//         reject()
+//         // inputUsd.value = "Что-то пошло не так!";
+//     }
+// });
+//     })
+// }
+// getData()
+// .then(() => {
+//     let data = JSON.parse(request.response);
+//     return data;
+// })
+// .then(() => {
+ 
     
+//         inputUsd.value = inputRub.value / data.usd;
+// })
+// .catch(() => inputUsd.value = "Что-то пошло не так!")
+
+// });
 
 
-    function getData() {
-        return new Promise(function (resolve, reject) {
-
-            request.open('GET', 'js/current.json');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-            
-
-
-            request.onreadystatechange = function () {
-                if (request.readyState < 4) {
-                    resolve()
-                } else if (request.readyState === 4) {
-                    if (request.status == 200 && request.status < 3)
-                        resolve()
-                } else {
-                    reject()
-                }
-            };
-
-            
-        
-
-            request.send();
-            
-        });
-
-    };
-
-    getData()
-    
-        .then(() => {
-            inputUsd.value = "Что-то пошло  так!"
-        })
-        .catch(() =>  inputUsd.value = "Что-то пошло не так!")
-});
+function doSomething() {
+    return new Promise((resolve, reject) => {
+      console.log("Готово.");
+      // Успех в половине случаев.
+      if (Math.random() > .5) {
+        resolve("Успех")
+      } else {
+        reject("Ошибка")
+      }
+    })
+  }
+  
+ 
+  doSomething().then(successCallback, failureCallback);
