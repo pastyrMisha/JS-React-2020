76276148c333ea777191161b0f6b4d39e737a937
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-18 21:03:15 +0300

    welldone

--- 032 homework PROMISE (converter usd)/js/script.js
+++ 032 homework PROMISE (converter usd)/js/script.js
@@ -21,18 +21,23 @@ inputRub.addEventListener('input', () => {
 
 
             request.addEventListener('readystatechange', function () {
+
                 // if (request.readyState === 4 && request.status == 200) {
                 //     resolve(JSON.parse(request.response));
                 // } else {
                 //     reject("Что-то пошло не так!")
                 // }
+                
                 request.onreadystatechange = function () {
                     if (request.readyState < 4) {
+                        // console.log('1');
                         resolve(JSON.parse(request.response))
                     } else if (request.readyState === 4) {
-                        if (request.status == 200 && request.status < 3)
-                            resolve(JSON.parse(request.response))
+                        if (request.status == 200 && request.status < 3) {
+                            // console.log('2');
+                            resolve(JSON.parse(request.response)) }
                     } else {
+                        // console.log('3');
                         reject("Что-то пошло не так!")
                     }
                 }
