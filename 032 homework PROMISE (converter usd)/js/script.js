6ba04eb86c91c06f071597f649c09c1546fa0d2e
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-18 14:36:28 +0300

    start

--- 032 homework PROMISE (converter usd)/js/script.js
+++ 032 homework PROMISE (converter usd)/js/script.js
@@ -1,64 +1,60 @@
 
-// let inputRub = document.getElementById('rub'),
-// inputUsd = document.getElementById('usd');
+let inputRub = document.getElementById('rub'),
+inputUsd = document.getElementById('usd');
 
 
 
 
 
-// inputRub.addEventListener('input', () => {
-// let request = new XMLHttpRequest();
+inputRub.addEventListener('input', () => {
+let request = new XMLHttpRequest();
 
 
-// function getData() {
+function getData() {
 
-//     return new Promise(function (resolve, reject) {
-
-
-// request.open('GET', 'js/current.json');
-// request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-// request.send();
+    return new Promise(function (resolve, reject) {
 
 
+request.open('GET', 'js/current.json');
+request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+request.send();
 
-// request.addEventListener('readystatechange', function() {
-//     if (request.readyState === 4 && request.status == 200) {
-//         // let data = JSON.parse(request.response);
-//         // inputUsd.value = inputRub.value / data.usd;
-//         resolve()
-//     } else {
-//         reject()
-//         // inputUsd.value = "Что-то пошло не так!";
-//     }
-// });
-//     })
-// }
-// getData()
-// .then(() => {
-//     let data = JSON.parse(request.response);
-//     return data;
-// })
-// .then(() => {
+
+
+request.addEventListener('readystatechange', function() {
+    if (request.readyState === 4 && request.status == 200) {
+        // let data = JSON.parse(request.response);
+        // inputUsd.value = inputRub.value / data.usd;
+        resolve(JSON.parse(request.response))
+    } else {
+        reject()
+        // inputUsd.value = "Что-то пошло не так!";
+    }
+});
+    })
+}
+getData()
+.then((data) => {
  
     
-//         inputUsd.value = inputRub.value / data.usd;
-// })
-// .catch(() => inputUsd.value = "Что-то пошло не так!")
+        inputUsd.value = inputRub.value / data.usd;
+})
+.catch(() => inputUsd.value = "Что-то пошло не так!")
 
-// });
+});
 
 
-function doSomething() {
-    return new Promise((resolve, reject) => {
-      console.log("Готово.");
-      // Успех в половине случаев.
-      if (Math.random() > .5) {
-        resolve("Успех")
-      } else {
-        reject("Ошибка")
-      }
-    })
-  }
+// function doSomething() {
+//     return new Promise((resolve, reject) => {
+//       console.log("Готово.");
+//       // Успех в половине случаев.
+//       if (Math.random() > .5) {
+//         resolve("Успех")
+//       } else {
+//         reject("Ошибка")
+//       }
+//     })
+//   }
   
  
-  doSomething().then(successCallback, failureCallback);
+//   doSomething().then(successCallback, failureCallback);
