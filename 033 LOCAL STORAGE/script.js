dd0a8dc4445721d593e38a3a5a73b822a6b02bfb
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-24 23:30:24 +0300

    welldone

--- 033 LOCAL STORAGE/script.js
+++ 033 LOCAL STORAGE/script.js
@@ -7,9 +7,37 @@
 // localStorage.clear();
 
 window.addEventListener('DOMContentLoaded', function() {
-    let checkbox = document.getElementById('rememberMe');
+    let checkbox = document.getElementById('rememberMe'),
+        change =document.getElementById('change'),
+        form = document.getElementsByClassName('wrapper-content')[0];
+
+    if(localStorage.getItem('IsChecked') == 'true') {
+        checkbox.checked = true;
+    };
+
+    if(localStorage.getItem('bg') == 'changed') {
+        form.style.backgroundColor = "#FF4766";
+    };
+
+    
 
     checkbox.addEventListener('click', function() {
         localStorage.setItem('IsChecked', true);
-    })
+    });
+
+    change.addEventListener('click', function() {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = "#FF4766";
+    });
+
+    let persone = {
+        name: "Alex",
+        age: 25,
+        tech: ["mobile", "notebook"]
+    }
+
+    let serializedPersone = JSON.stringify(persone);
+    localStorage.setItem("Alex", serializedPersone);
+
+    console.log(JSON.parse(localStorage.getItem("Alex")));
 })
