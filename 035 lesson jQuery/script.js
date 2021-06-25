b85a4d94fbc2583b31c52be8aaae7a91f9a6edf0
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-25 23:57:25 +0300

    welldone

--- 035 lesson jQuery/script.js
+++ 035 lesson jQuery/script.js
@@ -19,3 +19,31 @@ $(document).ready(function() {
         );
     });
 })
+
+// document.querySelectorAll('.list-item').forEach   — вот, что в JS заменяет значек $ (!)
+// .classList   — у JS теперь можно также тоглить классы (!) 
+// .addEventListener   — раньше его не было, а был у jQuery только .on()
+// $.ajax — этот метод работал у jQuery с запросами, теперь у JS есть .fetch
+//  .fadeToggle , .animate — любили jQuery за создание анимаций до тех пор, пока не появился CSS3, в котором без JS уже можно подключать различные анимации (!). Так же теперь в JS есть и метод .animate() тоже, но, пока, поддерживается не всеми браузерами.
+
+// Angular:
+
+// 1) node -> npm
+// 2) TypeScript
+// 3) Webpack
+// 4) MVC (model view controler)
+// 5) Angular
+
+// React:
+
+// 1) node -> npm
+// 2) Babel
+// 3) JSX
+// 4) React
+// 5) Webpack
+
+// Vue:
+
+// 1) node -> npm
+// 2) Babel
+// 3) Webpack
