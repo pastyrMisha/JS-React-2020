3f5bf04f6d5ccdb35556efa041e744543b69f378
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-25 22:38:38 +0300

    process

--- 035 lesson jQuery/script.js
+++ 035 lesson jQuery/script.js
@@ -5,6 +5,17 @@
 
 $(document).ready(function() {
     $('.list-item:first').hover(function() {
-        $(this).toogleClass('active');
+        $(this).toggleClass('animate__heartBeat');
+    });
+    $('.list-item:eq(2)').on('click', function() {
+        $('.image:even').fadeToggle('slow');
+    });
+    $('.list-item:eq(4)').on('click', function() {
+        $('.image:odd').animate(
+            {
+                opacity: 'toggle',
+                height: 'toggle'
+            }, 3000
+        );
     });
 })
