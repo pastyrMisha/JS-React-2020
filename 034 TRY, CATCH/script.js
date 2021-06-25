03245ffff6ca4ec6a1c00e9f4823a515f575579c
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-25 17:30:30 +0300

    well done

--- 034 TRY, CATCH/script.js
+++ 034 TRY, CATCH/script.js
@@ -1,5 +1,24 @@
+let json = '{"id":2}'
+
 try {
-    console.log('Normal');
-} catch {
-    console.log('error');
+    // console.log('Начинаем работу');
+    // console.log(a);
+    // console.log('Результат');
+    let user = JSON.parse(json);
+    // let user = JSON.parse(data); // вдрун пришло то, что нас не интересует, то catch
+    console.log(user);
+
+    if(!user.name) {
+        throw new Error("В этих данных нет имени!")
+    }
+} catch(error) {
+    console.log(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+
+    console.log(`Мы получили ошибку: ${error.name}`);
+} finally {
+    console.log('А я выполнюсь всегда!');
 }
+
+console.log('А я буду работать дальше');
