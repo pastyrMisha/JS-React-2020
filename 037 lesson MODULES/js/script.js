143d2f1256b6d5edbd743a58ba113122493869b6
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-06-26 22:56:22 +0300

    well done

--- 037 lesson MODULES/js/script.js
+++ 037 lesson MODULES/js/script.js
@@ -1,4 +1,56 @@
-let app = {
-    data: 45
-};
-console.log(app);
+// let app = {
+//     data: 45
+// };
+// console.log(app);
+
+// let number = 1;
+
+
+
+
+
+
+// // Инкапсуляция. Первый способ. Простой модуль - анонимная самовызывающаяся функция:
+
+// (function() {
+//     let number = 2;
+//     console.log(number);
+
+//     return console.log(number + 3);
+// }());
+
+// console.log(number);
+
+
+// Инкапсуляция. Второй способ:
+
+// let user = (function(){
+//     let private = function() {
+//         console.log('I am privat');
+//     }
+//     return {
+//         sayHello: function() {
+//             console.log('Hello');
+//         }
+//     };
+// }());
+
+// console.log(user);
+// console.log(user.sayHello());
+
+// Инкапсуляция. Третий способ:
+
+let user = (function(){
+    let private = function() {
+        console.log('I am privat');
+    }
+    let sayHello = function() {
+        console.log('Hello');
+    }
+    return {
+        sayHello: sayHello
+        };
+}());
+
+console.log(user);
+console.log(user.sayHello());
