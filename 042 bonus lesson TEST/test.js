1bdbfbc8f0f50e7739dd8d3537eafe531fd200c8
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-07-02 11:25:32 +0300

    start

---
+++ 042 bonus lesson TEST/test.js
@@ -0,0 +1,21 @@
+// Mocha — нужна, чтобы сами тесты работали, это основа
+// Chai — Это дополнение к ней, которое добавляет полезные функции для проверок, причем, в трех разных стилях
+
+
+function sayName(name) {
+    let message = "My name is " + name;
+    return message
+}
+
+let arr = [5, -3, 6, -5, 0, -7, 8, 9]
+let result = arr.reduce(function(sum,elem) {
+    return sum + elem;
+});
+
+let assert = require('chai').assert
+
+describe("sayName", function() {
+    it("Получаем фразу с новым именем", function(){
+        assert.typeOf(sayName("Alex"), 'string')
+    });
+});
