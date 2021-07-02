b0d73f1e083d495fce6d1cbb02526177d19cae86
Mikhail Zvendinov <34459362+pastyrMisha@users.noreply.github.com>
2021-07-02 15:20:19 +0300

    done

--- 042 bonus lesson TEST/test.js
+++ 042 bonus lesson TEST/test.js
@@ -7,7 +7,7 @@ function sayName(name) {
     return message
 }
 
-let arr = [5, -3, 6, -5, 0, -7, 8, 9]
+let arr = [5, -3, 6, -5, 0, -7, 8, 10] // заменили 9 на 10 и ошибка в проверке 2) assert.equal(result, 13)
 let result = arr.reduce(function(sum,elem) {
     return sum + elem;
 });
@@ -15,7 +15,14 @@ let result = arr.reduce(function(sum,elem) {
 let assert = require('chai').assert
 
 describe("sayName", function() {
+
     it("Получаем фразу с новым именем", function(){
         assert.typeOf(sayName("Alex"), 'string')
+    });
+});
+describe("arr", function() {
+
+    it("Получаем сумму чисел массива", function() {
+        assert.equal(result, 13)
     });
 });
